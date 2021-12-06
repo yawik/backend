969a042043c73c4740dad36527712f49162899e6
Carsten Bleek <bleek@cross-solution.de>
2021-12-06 16:20:44 +0000

    add staging

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,9 @@ module.exports = {
     env: {
       NODE_ENV: "development"
     },
+    env_staging: {
+      NODE_ENV: "staging"
+    },
     env_production: {
       NODE_ENV: "production"
     }
@@ -17,8 +20,18 @@ module.exports = {
       host : 'api.yawik.org',
       ref  : 'origin/main',
       repo : 'https://gitlab.com/yawik/backend.git',
-      path : '/home/yawik/backend',
+      path : '/home/yawik/production',
       'pre-deploy-local': 'ls -l',
+      'post-deploy' : 'yarn && NODE_ENV=production pm2 reload ecosystem.config.js --env production',
+      'pre-setup': 'pm2 ps'
+    },
+    staging : {
+      user : 'yawik',
+      host : 'api.yawik.org',
+      ref  : 'origin/staging',
+      repo : 'https://gitlab.com/yawik/backend.git',
+      path : '/home/yawik/staging',
+      'pre-deploy-local': 'echo STAGING',
       'post-deploy' : 'yarn && NODE_ENV=production pm2 reload ecosystem.config.js --env production',
       'pre-setup': 'pm2 ps'
     }
