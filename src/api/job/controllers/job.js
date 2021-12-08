5485b2bee1cd3d8bbeeb10e2e598167165c8e814
Carsten Bleek <bleek@cross-solution.de>
2021-12-08 08:52:18 +0100

    reformat code. Sets different error codes

--- src/api/job/controllers/job.js
+++ src/api/job/controllers/job.js
@@ -25,12 +25,13 @@ module.exports = createCoreController("api::job.job", ({ strapi }) => ({
         if (!userData || userData.data && userData.data.error) {
           return {
             error: {
-              status: 0,
+              status: 4001,
               name: "invalid_token",
               message: "Token verification failed",
             },
           };
         }
+
         const {
           title,
           dateCreated,
@@ -40,79 +41,93 @@ module.exports = createCoreController("api::job.job", ({ strapi }) => ({
           job,
           publishedAt,
         } = ctx.request.body.data;
+
+        if (!userData.jobId) {
+          return {
+            error: {
+              status: 4002,
+              name: "no_job_id",
+              message: "Request requires a uuid jobId",
+            },
+          };
+        }
+
         let newJob = {
           data: {
             title: title,
             dateCreated: dateCreated || "2021-12-03T15:50:41.398Z",
             dateModified: dateModified || "2021-12-03T15:50:41.398Z",
             dateDeleted: dateDeleted || "2021-12-03T15:50:41.398Z",
-            jobId: jobId || "12",
+            jobId: jobId,
             job: job || "Bilal",
             publishedAt: publishedAt || "2021-12-03T15:50:41.398Z",
           },
         };
-          const sub =  userData.data.sub;
-          let isUserExist =await strapi.service('api::job-user.job-user').findOneUser({sub: sub});
-          if (!isUserExist || !(isUserExist.length > 0)) {
-              if (userData?.data?.sub) {
-                  userData.data.publishedAt = newJob.data.publishedAt;
-                  let newUserCreated =await strapi.query('api::job-user.job-user').create({ data: userData.data });
-                  if (!newUserCreated) {
-                    return {
-                        error: {
-                          status: 0,
-                          name: "ERROR",
-                          message: "UnAuthorization",
-                        },
-                      };
-                  } else {
-                    newJob.data.jobUser = newUserCreated.id;
-                    let jobs = await strapi.query("api::job.job").create(newJob);
-                    return jobs;
-                  }
-              } else {
-                return {
-                    error: {
-                      status: 0,
-                      name: "UnAuthorization",
-                      message: "UnAuthorization",
-                    },
-                  };    
-              }
-          } else { // title: newJob?.data?.title, jobId: newJob?.data?.title
-              let title = newJob.data.title;
-              let jobId = newJob.data.jobId;
-              console.log('user  found', title, jobId)
-              let isJobExist = await strapi.service("api::job.job").JobFindOne({title:title, jobId:jobId});
-              if (isJobExist && isJobExist.length > 0) {
-                  return {
-                      error: {
-                          status: 0,
-                          name: "Job Already Exist",
-                          message: "Job Already Exist",
-                      },
-                  };
-              } else {
-                  newJob.data.jobUser = isUserExist[0].id;
-                  let jobs = await strapi.query("api::job.job").create(newJob);
-                  return jobs;
-              }
+
+        const sub =  userData.data.sub;
+        let isUserExist =await strapi.service('api::job-user.job-user').findOneUser({sub: sub});
+
+        if (!isUserExist || !(isUserExist.length > 0)) {
+          if (userData?.data?.sub) {
+            userData.data.publishedAt = newJob.data.publishedAt;
+            let newUserCreated =await strapi.query('api::job-user.job-user').create({ data: userData.data });
+            if (!newUserCreated) {
+              return {
+                  error: {
+                    status: 4002,
+                    name: "user_creation_failed",
+                    message: "Could not create User",
+                  },
+                };
+            } else {
+              newJob.data.jobUser = newUserCreated.id;
+              let jobs = await strapi.query("api::job.job").create(newJob);
+              return jobs;
+            }
+          } else {
+            return {
+                error: {
+                  status: 0,
+                  name: "UnAuthorization",
+                  message: "UnAuthorization",
+                },
+              };    
           }
+        } else { // title: newJob?.data?.title, jobId: newJob?.data?.title
+          let title = newJob.data.title;
+          let jobId = newJob.data.jobId;
+          console.log('user  found', title, jobId)
+          let isJobExist = await strapi.service("api::job.job").JobFindOne({jobId:jobId});
+          if (isJobExist && isJobExist.length > 0) {
+            // todo update job
+            return {
+                error: {
+                    status: 0,
+                    name: "Job Already Exist",
+                    message: "Job Already Exist",
+                },
+            };
+          } else {
+            newJob.data.jobUser = isUserExist[0].id;
+            let jobs = await strapi.query("api::job.job").create(newJob);
+            return jobs;
+          }
+        }
         
       } else {
         return {
           error: {
-            status: 0,
-            name: "UnAuthorization",
-            message: "UnAuthorization",
+            status: 4004,
+            name: "keycloak_unavailable",
+            message: "Cannot access Keycloak",
           },
         };
       }
     } catch (e) {
       return {
         error: {
-          status: 0,
-          name: "UnAuthorization",
+          status: 5000,
+          name: "internal_error",
           message: "UnAuthorization",
         },
       };
