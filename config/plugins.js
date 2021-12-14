035d24bd360d403fa7dd941c1057f3a003005367
Asrar Memon <asrar.memon0001@gmail.com>
2021-12-15 00:15:09 +0530

    bug solved in email

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,21 @@
 module.exports = ({ env }) => ({
   email: {
-    provider: 'strapi-provider-email-smtp',
-    providerOptions: {
-      host: env('EMAIL_SMTP_HOST', 'mc.cross-solution.de'), //SMTP Host
-      port: env('EMAIL_SMTP_PORT', 465)   , //SMTP Port
-      secure: env('EMAIL_SMTP_SECURE', true),
-      username: env('EMAIL_SMTP_USERNAME', ''),
-      password: env('EMAIL_SMTP_PASSWORD', ''),
-      rejectUnauthorized: true,
-      requireTLS: true,
-      connectionTimeout: 1,
-    },
-    settings: {
-      from: env('EMAIL_SETTINGS_FROM', '' ),
-      replyTo: env('EMAIL_SETTINGS_REPLYTO', '' ),
-    },
+    config: {
+      provider: 'strapi-provider-email-smtp',
+      providerOptions: {
+        host: env('EMAIL_SMTP_HOST', 'mc.cross-solution.de'), //SMTP Host
+        port: env('EMAIL_SMTP_PORT', 465), //SMTP Port
+        secure: env('EMAIL_SMTP_SECURE', true),
+        username: env('EMAIL_SMTP_USERNAME', ''),
+        password: env('EMAIL_SMTP_PASSWORD', ''),
+        rejectUnauthorized: true,
+        requireTLS: true,
+        connectionTimeout: 1,
+      },
+      settings: {
+        from: env('EMAIL_SETTINGS_FROM', ''),
+        replyTo: env('EMAIL_SETTINGS_REPLYTO', ''),
+      },
+    }
   },
 });
