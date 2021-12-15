69985b795ecd2d384eed40cc90dce20e58bcb7ec
Carsten Bleek <bleek@cross-solution.de>
2021-12-15 10:16:06 +0000

    move from and replyto from settings to options

--- config/plugins.js
+++ config/plugins.js
@@ -3,19 +3,17 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'strapi-provider-email-smtp',
       providerOptions: {
+        from: env('EMAIL_SETTINGS_FROM', ''),
         host: env('EMAIL_SMTP_HOST', 'mc.cross-solution.de'), //SMTP Host
         port: env('EMAIL_SMTP_PORT', 465), //SMTP Port
         secure: env('EMAIL_SMTP_SECURE', true),
         username: env('EMAIL_SMTP_USERNAME', ''),
         password: env('EMAIL_SMTP_PASSWORD', ''),
         rejectUnauthorized: true,
+        replyTo: env('EMAIL_SETTINGS_REPLYTO', ''),
         requireTLS: true,
         connectionTimeout: 1,
       },
-      settings: {
-        from: env('EMAIL_SETTINGS_FROM', ''),
-        replyTo: env('EMAIL_SETTINGS_REPLYTO', ''),
-      },
-    }
+    },
   },
 });
