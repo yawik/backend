dec4ab852d5914ba8788a7fade63fb6c5ad5c3c9
Carsten Bleek <bleek@cross-solution.de>
2021-12-30 08:56:58 +0000

    add organization model

---
+++ src/extensions/documentation/server/config/default-config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  info: {
+    title: 'My Title',
+    description: '',
+    termsOfService: 'YOUR_TERMS_OF_SERVICE_URL',
+    contact: {
+      name: 'TEAM',
+      email: 'contact-email@something.io',
+      url: 'mywebsite.io',
+    },
+    license: {
+      name: 'Apache 2.0',
+      url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
+    },
+  },
+}
