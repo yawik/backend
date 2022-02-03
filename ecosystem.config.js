df946c863f3bd693ddfc24100836a190d9b1eb75
Carsten Bleek <bleek@cross-solution.de>
2022-02-03 18:22:22 +0000

    add local pm2 to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,13 @@ module.exports = {
     script: 'yarn',
     args: 'start',
     env: {
+      NODE_ENV: "development",
+      PORT: 1337
+    },
+    env_production: {
       NODE_ENV: "production",
       PORT: 3000
-    },
+    }
   }],
   
   deploy : {
@@ -18,6 +22,15 @@ module.exports = {
       path : '/home/yawik/api.yawik.org',
       'post-deploy' : 'NODE_ENV=production yarn && NODE_ENV=production yarn build && pm2 reload ecosystem.config.js --env production -- --port 3000',
       'pre-setup': 'pm2 ps'
-    }
+    },
+    development: {
+      user : 'yawik',
+      host : 'localhost',
+      ref  : 'origin/main',
+      repo : 'https://gitlab.com/yawik/backend.git',
+      path : '/home/strapi/pm2',
+      'post-deploy' : 'yarn && yarn build && pm2 reload',
+      'pre-setup': 'pm2 ps'
+    }    
   }
 };
