31cd15bac195ab02a30f7b3855e0115064f9bb2a
Carsten Bleek <bleek@cross-solution.de>
2022-03-15 21:33:56 +0100

    chore: use @strapi/provider-email-nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,23 @@
 module.exports = ({ env }) => ({
   email: {
     config: {
-      provider: 'strapi-provider-email-smtp',
+      provider: 'nodemailer',
       providerOptions: {
-        from: env('EMAIL_SETTINGS_FROM', ''),
         host: env('EMAIL_SMTP_HOST', 'mc.cross-solution.de'), //SMTP Host
         port: env('EMAIL_SMTP_PORT', 465), //SMTP Port
         secure: env('EMAIL_SMTP_SECURE', true),
-        username: env('EMAIL_SMTP_USERNAME', ''),
-        password: env('EMAIL_SMTP_PASSWORD', ''),
+        auth: {
+          user: env('EMAIL_SMTP_USERNAME', ''),
+          pass: env('EMAIL_SMTP_PASSWORD', ''),
+        },
         rejectUnauthorized: true,
-        replyTo: env('EMAIL_SETTINGS_REPLYTO', ''),
         requireTLS: true,
-        connectionTimeout: 1,
       },
+      settings: {
+        defaultFrom: env('EMAIL_SETTINGS_FROM', ''),
+        defaultReplyTo: env('EMAIL_SETTINGS_REPLYTO', ''),
+      },
+      
     },
   },
   upload: {
