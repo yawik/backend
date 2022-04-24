184671af835a5b869da51575f5a6da091a2b2a51
Carsten Bleek <bleek@cross-solution.de>
2022-04-24 11:41:17 +0200

    set status tp unpublished

--- config/cron.js
+++ config/cron.js
@@ -8,9 +8,11 @@ module.exports = {
         await Promise.all(publishedJob.results.map(async (jobInfo) => {
             const id = jobInfo.id;
             if (moment.utc().diff(moment(jobInfo.publishedAt).utc(), "days") > jobInfo.meta.publishDays) {
+                console.log("CRON unpublish:", jobInfo.id);
                 await strapi.api.job.services.job.update(id,
                     {
                         data: {
+                            status: 'unpublished',
                             publishedAt: null
                         }
                     });
