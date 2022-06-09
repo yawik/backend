01bf02f74d6192b6ef29459982def249fbd87eab
Carsten Bleek <bleek@cross-solution.de>
2022-06-09 13:07:50 +0200

    remove scrapy collection

--- src/api/scrapy/controllers/scrapy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  scrapy controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::scrapy.scrapy');
--- src/api/scrapy/services/scrapy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * scrapy service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::scrapy.scrapy');
